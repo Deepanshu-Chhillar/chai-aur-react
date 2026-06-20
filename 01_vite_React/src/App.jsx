7db1729e5e29385db537264d135e9ba6b03c0bc3
deepanshu-chhilar <devildeep56t@gmail.com>
2026-06-20 23:11:32 +0530

    adding several files of react+vite works

---
+++ 01_vite_React/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./chai"
+
+function App() {
+ 
+
+  return (
+    <>      {/*this is called fragment in react jsx only want 1 element to return so we use this tag as in it it can has many elements init*/}
+      <Chai/>
+      <h3>Ya fragment is working without fragment its not working</h3>
+      <p>Tested with react jsx</p>
+    </>
+  )
+}
+
+export default App
---
+++ readme.md
@@ -0,0 +1,1 @@
+npx create-react-app   --  A utility cmd used in past years to get start with downloading a bundle to rect start but its heavy and take time to download. 
